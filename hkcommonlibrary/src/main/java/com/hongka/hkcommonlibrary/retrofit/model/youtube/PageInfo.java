c791124cebf0d7d499d4dcd495e747411d76f70f
jusung.kim <jusung.kim@sk.com>
2017-05-25 18:37:33 +0900

    테스트

--- hkcommonlibrary/src/main/java/com/hongka/hkcommonlibrary/retrofit/model/youtube/PageInfo.java
+++ hkcommonlibrary/src/main/java/com/hongka/hkcommonlibrary/retrofit/model/youtube/PageInfo.java
@@ -1,6 +1,7 @@
 package com.hongka.hkcommonlibrary.retrofit.model.youtube;
 
 public class PageInfo {
+	public String test = "사랑해요";
 	public String nextPageToken;
 	public String prevPageToken;
 	public int totalResults;
