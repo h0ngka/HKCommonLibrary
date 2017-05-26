9c01266e7cf7213aa32eb333d980afb0ff751e9c
jusung.kim <jusung.kim@sk.com>
2017-05-26 13:55:43 +0900

    Gradle build 추가

--- hkcommonlibrary/src/main/java/com/hongka/hkcommonlibrary/retrofit/model/youtube/PageInfo.java
+++ hkcommonlibrary/src/main/java/com/hongka/hkcommonlibrary/retrofit/model/youtube/PageInfo.java
@@ -1,7 +1,6 @@
 package com.hongka.hkcommonlibrary.retrofit.model.youtube;
 
 public class PageInfo {
-	public String test = "사랑해요ㅎㅎ";
 	public String nextPageToken;
 	public String prevPageToken;
 	public int totalResults;
