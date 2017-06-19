1af2bc90601e98d15361101a48fc58741037e02c
Kim Jusung <hongka@treasurehunter.co.kr>
2017-06-20 01:12:19 +0900

    Seriazable  적용

--- hkcommonlibrary/src/main/java/com/hongka/hkcommonlibrary/retrofit/model/youtube/ChannelsResponse.java
+++ hkcommonlibrary/src/main/java/com/hongka/hkcommonlibrary/retrofit/model/youtube/ChannelsResponse.java
@@ -1,12 +1,15 @@
 package com.hongka.hkcommonlibrary.retrofit.model.youtube;
 
+import android.os.Parcelable;
+
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by jusung.kim@sk.com on 2017/05/23
  */
 
-public class ChannelsResponse {
+public class ChannelsResponse implements Serializable {
     public PageInfo pageInfo;
     public List<Channel> items;
 }
